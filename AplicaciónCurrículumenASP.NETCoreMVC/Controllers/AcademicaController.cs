72af69fea41bfe62928eeed016aeca04812efade
Alda <rochaaldanafcojavier@gmail.com>
2025-05-26 00:27:06 -0600

    Proyecto CV

---
+++ AplicaciónCurrículumenASP.NETCoreMVC/Controllers/AcademicaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicaciónCurrículumenASP.NETCoreMVC.Controllers
+{
+    public class AcademicaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AplicaciónCurrículumenASP.NETCoreMVC/Controllers/LaboralController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicaciónCurrículumenASP.NETCoreMVC.Controllers
+{
+    public class LaboralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
