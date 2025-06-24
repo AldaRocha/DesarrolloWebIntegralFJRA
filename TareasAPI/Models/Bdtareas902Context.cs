e8528dbf00dac3aeec79507b68a8ecc240f3d249
Alda <rochaaldanafcojavier@gmail.com>
2025-06-23 18:49:20 -0600

    Tareas api

---
+++ TareasAPI/Models/Bdtareas902Context.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TareasAPI.Models;
+
+public partial class Bdtareas902Context : DbContext
+{
+    public Bdtareas902Context()
+    {
+    }
+
+    public Bdtareas902Context(DbContextOptions<Bdtareas902Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.TareaId).HasName("PK__Tarea__5CD83991BB4E3736");
+
+            entity.ToTable("Tarea");
+
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TareasAPI/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TareasAPI.Models;
+
+public partial class Tarea
+{
+    public int TareaId { get; set; }
+
+    public string? Nombre { get; set; }
+}
